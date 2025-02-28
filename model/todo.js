b1f4dc5bd23409d06588986c52f4d8b71711821e
emynado <joatradeemy@gmail.com>
2025-02-27 23:12:33 -0500

    Update todo.js

--- model/todo.js
+++ model/todo.js
@@ -3,3 +3,63 @@ import { PrismaClient } from "@prisma/client";
 
 // Créer une instance du client prisma
 const prisma = new PrismaClient();
+
+//Création des fonctions
+/**
+ * Permet d'ajouter une tâche à la liste des tâches
+ * @param {*} Element de la table todo
+ * @returns
+ */
+
+
+
+
+
+/**
+ * Obtenir la liste de toutes les tâches
+ * @returns la liste des tâches
+ */
+
+
+
+
+/**
+ * Pour selectionner une tâche par son id
+ * @param {*} id
+ * @returns la tâche
+ */
+
+
+
+/**
+ * Pour modifier l'état d'une tâches
+ * @param {*} id etat
+ * @returns
+ */
+
+
+
+
+
+/**
+ * Mise à jour d'une tâche
+ * @param {*} id Elements de la table todo
+ * @returns
+ */
+
+
+
+
+/**
+ * Suppresion d'une tâche
+ * @param {*} id
+ * @returns
+ */
+
+
+
+/**
+ * Pour ajouter dans l'historique
+ * @param {*} Element de la table todo
+ * @returns
+ */
